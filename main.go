d4f9b4e0c17bfeefc786cf33d71b5eb695eef57c
julaizoe <maoqishuxi@gmail.com>
2024-09-02 16:35:14 +0800

    ✨ feat(feature):更换到url

--- main.go
+++ main.go
@@ -6,52 +6,32 @@ import (
 	"log"
 	"math/rand"
 	"net/http"
-	"net/url"
 	uids2 "scripts/uids"
 	"time"
 )
 
 func main() {
-	// 目标 URL
-	targetURL := "https://weibo.julai.fun/api/weibo?uid="
-	// targetURL := "http://localhost:3000/api/weibo?uid="
-
-	// 代理 URL
-	proxyURL, err := url.Parse("http://192.168.0.208:1080")
-	// proxyURL, err := url.Parse("http://127.0.0.1:1080")
-	if err != nil {
-		log.Fatalf("Failed to parse proxy URL: %v", err)
-	}
-
-	// uids
-	//uids := []string{"1281503010", "1784977674", "1922411040", "2423296524", "1751162512", "1656918431"}
-
-	// 创建一个自定义的 HTTP 客户端，使用代理
-	transport := &http.Transport{
-		Proxy: http.ProxyURL(proxyURL),
-	}
-	client := &http.Client{
-		Transport: transport,
-	}
-
 	// 循环发送请求
 	for {
 		uids := uids2.GetUids()
 
-		for _, uid := range uids {
+		for _, uid := range uids.Data {
 			if !uid.Status {
 				continue
 			}
 
+			// 目标 URL
+			targetURL := fmt.Sprintf("https://weibo-nu.vercel.app/api/weibo?uid=%s&page=%d&count=%d", uid.UID, 1, 10)
+
 			fmt.Printf("%s start run %s %s \n", time.Now().Format("2006-01-02 15:04:05"), uid.ScreenName, uid.UID)
 			// 创建 HTTP 请求
-			req, err := http.NewRequest("GET", targetURL+uid.UID, nil)
+			req, err := http.NewRequest("GET", targetURL, nil)
 			if err != nil {
 				log.Fatalf("Failed to create HTTP request: %v", err)
 			}
 
 			// 发送 HTTP 请求
-			resp, err := client.Do(req)
+			resp, err := uids2.Proxy().Do(req)
 			if err != nil {
 				log.Printf("Failed to send HTTP request: %v", err)
 				continue
